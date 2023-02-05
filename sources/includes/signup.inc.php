95af07f8038fcb80b0f449e5d6ccc9467ad9a566
Vince Jeremy Ladion <55903157+vinceedesu@users.noreply.github.com>
2023-02-05 17:35:43 +0800

    123

---
+++ sources/includes/signup.inc.php
@@ -0,0 +1,47 @@
+<?php
+
+if (empty($_POST["name"])) {
+    die("Name is required");
+}
+if (empty($_POST["uid"])) {
+    die("Name is required");
+}
+
+if ( ! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+    die("Valid email is required");
+}
+
+if (strlen($_POST["pwd"]) < 8) {
+    die("Password must be at least 8 characters");
+}
+
+if ( ! preg_match("/[a-z]/i", $_POST["pwd"])) {
+    die("Password must contain at least one letter");
+}
+
+if ( ! preg_match("/[0-9]/", $_POST["pwd"])) {
+    die("Password must contain at least one number");
+}
+
+if ($_POST["pwd"] !== $_POST["pwd2"]) {
+    die("Passwords must match");
+}
+
+$password_hash = password_hash($_POST["pwd"], PASSWORD_DEFAULT);
+
+$mysqli = require __DIR__ . "/dbh-inc.php";
+
+$sql = "INSERT INTO user (name, email, password_hash)
+        VALUES (?, ?, ?)";
+        
+$stmt = $mysqli->stmt_init();
+
+if ( ! $stmt->prepare($sql)) {
+    die("SQL error: " . $mysqli->error);
+}
+
+$stmt->bind_param("sss",
+                  $_POST["name"],
+                  $_POST["email"],
+                  $_POST["uid"],
+                  $password_hash);
---
+++ sources/php/sign-up.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../style.css">
+    <link rel="stylesheet" href="../css/no-scroll.css">
+    <title>Sign Up</title>
+</head>
+<body>
+    <header>
+        <div class="logo"><span>Hopi Crafts</span></div>
+        <div class="nav-links"> 
+            <ul>
+                <li><a href="login.php">Back</a></li>
+    
+            </ul>
+        </div>
+    </header>
+        <div class="logIn">
+            <div class="text-content">
+                <h1>Sign Up</h1>
+                <p>Fill up the form wewe</p>
+            </div>
+            <section class="signUp-form">
+                <form action="../includes/signup.inc.php" method="post" novalidate>
+                <input type="text" id="name" name="name" placeholder="Full Name...">
+                <br>
+                <input type="text" id="uid" name="uid" placeholder="Username...">
+                <br>
+                <input type="email" id="email" name="email" placeholder="Email...">
+                <br>
+                <input type="password" id="pwd" name="pwd" placeholder="Password...">
+                <br>
+                <input type="password" id="pwd2" name="pwd2" placeholder="Repeat password...">
+                <br>
+                <button class="fillButton" type="submit" name="submit">Sign Up</button>
+                </form>
+
+            <?php 
+          
+        ?>
+            </section>
+        </div>
+</body>
+</html>
